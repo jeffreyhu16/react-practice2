608157f70a69a053d07691baec909eae334bfb82
Jeffrey <jeffreyhu16@gmail.com>
2022-02-23 09:28:09 +0800

    final update

--- src/App.js
+++ src/App.js
@@ -8,13 +8,8 @@ export default function App() {
     const cardData = data.map(card => {
         return (
             <Card
-                img={card.coverImg}
-                rating={card.stats.rating}
-                reviewCount={card.stats.reviewCount}
-                location={card.location}
-                title={card.title}
-                price={card.price}
-                star={card.star}
+                key={card.id}
+                {...card}
             />
         )    
     });
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,24 @@
 import React from 'react'
-// import star from '../images/star.png'
+import data from '../data'
 
 export default function Card(props) {
+    // const { openSpots, location, img, title, price, star } = props;
 
+    // console.log(openSpots);
+
+    let stockQuant = '';
+    if (!props.openSpots) {
+        stockQuant = 'SOLD OUT'
+    } else if (props.location === 'Online') {
+        stockQuant = 'ONLINE'
+    }
 
     return (
         <div className="card-component">
-            <img src={`../images/${props.img}`} className="card-img" />
+            {stockQuant && <div className="card-badge">{stockQuant}</div>}
+            <img src={`../images/${props.coverImg}`} className="card-img" />
             <h2 className="card-title">{props.title}</h2>
-            <p className="card-price"><strong>From ${props.price}</strong> / person</p>
+            <p className="card-price"><strong>From {props.price}</strong> / person</p>
             <div className="rating">
                 <img src={`../images/${props.star}`} className="star-img" />
                 <p className="rating-info">5.0 (6) USA</p>
